9a40b9011e966f26ce8a0783c0ab6ecc7ada0b52
jay sojitra <108830016+mrjaysojitra@users.noreply.github.com>
2024-02-20 17:53:48 +0530

    Add files via upload

---
+++ first/src/first/ThirdProgram.java
@@ -0,0 +1,14 @@
+package first;
+
+public class ThirdProgram {
+	
+	// ternary operator
+public static void main(String[] args) {
+		
+	int x = 20;
+			
+	String msg = (x%2==0) ? "odd": "even";  
+	System.out.println("Value of x is: " +  msg);  
+	}
+
+}
