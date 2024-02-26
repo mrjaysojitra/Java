eff89bb62736939ed81c305e1428be6c4c337c31
jay sojitra <108830016+mrjaysojitra@users.noreply.github.com>
2024-02-26 14:33:34 +0530

    Add files via upload

---
+++ daytwo/Employee.java
@@ -0,0 +1,35 @@
+package daytwo;
+
+public class Employee {
+		
+		private String name , designation , email;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getDesignation() {
+			return designation;
+		}
+
+		public void setDesignation(String designation) {
+			this.designation = designation;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		@Override
+		public String toString() {
+			return "DayTwo [name=" + name + ", designation=" + designation + ", email=" + email + "]";
+		}
+}
---
+++ daytwo/Employee_Main.java
@@ -0,0 +1,8 @@
+package daytwo;
+
+public class Employee_Main {
+	public static void Main(String args[]) {
+		Employee emp= new Employee();
+		emp.setName("Jay");
+	}
+}
